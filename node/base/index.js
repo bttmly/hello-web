fe5aa68cf2b783598fef1868f5bf72c2154e8282
Nick Bottomley <nhbottomley@gmail.com>
2015-03-29 14:10:06 -0700

    early return node

--- node/base/index.js
+++ node/base/index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
   if (req.url === "/") {
-    res.end(JSON.stringify({message: "Hello, World!"}));
+    return res.end(JSON.stringify({message: "Hello, World!"}));
   }
   res.writeHead(404);
   res.end(JSON.stringify({error: "Not found"}));
