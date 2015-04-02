aad304ae89a2e83132495d0de17ee63e802726dd
jlburkhead <jlburkhead@ucdavis.edu>
2015-04-02 13:56:10 -0700

    fix flask 404s

--- python/flask/app.py
+++ python/flask/app.py
@@ -3,14 +3,18 @@ import json
 
 app = flask.Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-  return flask.Response(json.dumps({"message": "Hello, World!"}), mimetype="application/json")
+    return flask.Response(json.dumps({"message": "Hello, World!"}),
+                          mimetype="application/json")
 
-# seem to be getting 500s instead of handling 404s...
+
 @app.errorhandler(404)
-def page_not_found():
-  flask.Response(json.dumps({"error": "Not found"}), mimetype="application/json")
+def page_not_found(error):
+    return flask.Response(json.dumps({"error": "Not found"}),
+                          status=404,
+                          mimetype="application/json")
 
 if __name__ == "__main__":
-  app.run(None, 8080)
+    app.run(None, 8080)
